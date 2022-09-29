a2f6c0de1ad17d8b2a44d519b665ad7b8f21f3a6
yux-m <yuxmeng@seas.upenn.edu>
2022-09-29 11:03:26 -0400

    搞不赢 还是返回57了

--- schemas.js
+++ schemas.js
@@ -1,29 +1,4 @@
-const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
-
-const extension = (joi) => ({
-  type: 'string',
-  base: joi.string(),
-  messages: {
-    'string.escapeHTML': '{{#label}} must not include HTML!'
-  },
-  rules: {
-    escapeHTML: {
-      validate(value, helpers) {
-        const clean = sanitizeHtml(value, {
-          allowedTags: [],
-          allowedAttributes: {},
-        });
-        if (clean !== value) return helpers.error('string.escapeHTML', {
-          value
-        })
-        return clean;
-      }
-    }
-  }
-});
-
-const Joi = BaseJoi.extend(extension)
+const Joi = require('joi');
 
 module.exports.spotSchema = Joi.object({
   spot: Joi.object({
